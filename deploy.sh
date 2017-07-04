be72770873e1c4b6ea9e6458382644cd9f116c94
constantm <constantmeiring@gmail.com>
2017-07-04 10:01:10 +0200

    Add custom domain warning to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 git push origin `git subtree split --prefix dist master`:gh-pages --force
+echo "\n\n!!! Remember to add custom domain back at https://github.com/constantm/constant.co.za/settings"
